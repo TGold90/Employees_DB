ae3e8dfae5e39d6b4de9f9cca1b22b33e2507f3e
TGOLD <thomaswgold90@gmail.com>
2022-09-01 22:04:08 -0600

    init

---
+++ db/schema.sql
@@ -0,0 +1,29 @@
+DROP DATABASE IF EXISTS org_db;
+CREATE DATABASE org_db;
+
+USE org_db;
+
+CREATE TABLE department (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  dept_name VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE role (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL NOT NULL,
+  department_id INT
+  FOREIGN KEY (department_id)
+  REFERENCES department (id)
+  ON DELETE SET NULL
+);
+
+CREATE TABLE employee (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INT NOT NULL,
+  manager_id INT
+  FOREIGN KEY (role_id)
+  REFERENCES role (id)
+);
---
+++ db/seeds.sql
@@ -0,0 +1,17 @@
+INSERT INTO department (dept_name)
+VALUES ("Merchandising"),
+       ("Sales"),
+       ("Sourcing")
+
+INSERT INTO role (title, salary)
+VALUES ("Junior Merchant", 45000),
+       ("Partner Manager", 75000),
+       ("Sourcing Agent", 65000)
+       
+       
+
+INSERT INTO employee (first_name, last_name, manager_id)
+VALUES ("Melissa", "McFarlane", 3),
+       ("Becky", "Morrison", 4), 
+       ("Grace", "Seal", 5)
+       
