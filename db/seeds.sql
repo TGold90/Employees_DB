c736a3b31e4ff5379f2e895b796352388eadb94c
TGOLD <thomaswgold90@gmail.com>
2022-09-03 13:25:06 -0600

    addDepartment and addRole functions

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,17 +1,17 @@
 INSERT INTO department (dept_name)
 VALUES ("Merchandising"),
        ("Sales"),
-       ("Sourcing")
+       ("Sourcing");
 
-INSERT INTO role (title, salary)
-VALUES ("Junior Merchant", 45000),
-       ("Partner Manager", 75000),
-       ("Sourcing Agent", 65000)
+INSERT INTO role (title, salary, department_id)
+VALUES ("Manager", 45000, 1),
+       ("Partner Manager", 75000, 2),
+       ("Sourcing Agent", 65000, 3);
        
        
 
-INSERT INTO employee (first_name, last_name, manager_id)
-VALUES ("Melissa", "McFarlane", 3),
-       ("Becky", "Morrison", 4), 
-       ("Grace", "Seal", 5)
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Melissa", "McFarlane", 1, null),
+       ("Becky", "Morrison", 2, 1), 
+       ("Grace", "Seal", 3, 1);
        
---
+++ server.js
@@ -0,0 +1,177 @@
+const mysql = require("mysql2");
+const inquirer = require("inquirer");
+const { listen } = require("express/lib/application");
+
+const db = mysql.createConnection(
+  {
+    host: "localhost",
+    // MySQL username,
+    user: "root",
+    // MySQL password
+    password: "tomtomclub13",
+    database: "org_db",
+  }
+  // console.log(`Connected to the org_db database.`)
+);
+db.connect((err) => {
+  if (err) {
+    throw err;
+  }
+});
+
+const menu = async () => {
+  const answer = await inquirer.prompt([
+    {
+      type: "list",
+      name: "choice",
+      choices: [
+        "View All Employees",
+        "View All Departments",
+        "View All Roles",
+        "Add an Employee",
+        "Add a Department",
+        "Add a Role",
+        "Update an Employee Role",
+        "Exit",
+      ],
+    },
+  ]);
+  switch (answer.choice) {
+    case "View All Employees":
+      viewAllEmployees();
+      break;
+    case "View All Departments":
+      viewAllDepartments();
+      break;
+    case "View All Roles":
+      viewAllRoles();
+      break;
+    case "Add an Employee":
+      addEmployee();
+      break;
+    case "Add a Department":
+      addDepartment();
+      break;
+    case "Add a Role":
+      addRole();
+      break;
+    case "Update an Employee Role":
+      updateRole();
+      break;
+    default:
+      process.exit();
+  }
+};
+
+const viewAllEmployees = async () => {
+  try {
+    const data = await db.promise().query(`SELECT * FROM employee`);
+    console.table(data[0]);
+    menu();
+  } catch (err) {
+    console.log(err);
+  }
+};
+const viewAllDepartments = async () => {
+  try {
+    const data = await db.promise().query(`SELECT * FROM department`);
+    console.table(data[0]);
+    menu();
+  } catch (err) {
+    console.log(err);
+  }
+};
+const viewAllRoles = async () => {
+  try {
+    const data = await db.promise().query(`SELECT * FROM role`);
+    console.table(data[0]);
+    menu();
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const addEmployee = async () => {
+  try {
+    //creates array with different roles listed. have to declare first index to use.
+    const roles = await db
+      .promise()
+      .query(`SELECT title AS name, id AS value FROM role`);
+    console.log(roles[0]);
+    //
+    const managers = await db
+      .promise()
+      .query(`SELECT first_name AS name, id AS value FROM employee`);
+    const employeeInfo = await inquirer.prompt([
+      {
+        type: "input",
+        name: "first_name",
+        message: "What is the employee's first name?",
+      },
+      {
+        type: "input",
+        name: "last_name",
+        message: "What is the employee's last name?",
+      },
+      {
+        type: "list",
+        name: "role_id",
+        choices: roles[0],
+        message: "What is the employee's role?",
+      },
+      {
+        type: "list",
+        name: "manager_id",
+        choices: managers[0],
+        message: "Who is the employee's manager?",
+      },
+    ]);
+    const data = await db
+      .promise()
+      .query("INSERT INTO employee SET ?", employeeInfo);
+    console.log("Employee succesfully added!");
+    viewAllEmployees();
+  } catch (error) {
+    console.log(error);
+  }
+};
+//ADD DEPARTMENT FUNCTION
+const addDepartment = async () => {
+  try {
+    const departmentInfo = await inquirer.prompt([
+      {
+        type: "input",
+        name: "dept_name",
+        message: "What is the department name?",
+      },
+    ]);
+    const data = await db
+      .promise()
+      .query("INSERT INTO department SET ?", departmentInfo);
+    console.log("Department succesfully added!");
+    viewAllDepartments();
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const addRole = async () => {
+  try {
+    const roleInfo = await inquirer.prompt([
+      {
+        type: "input",
+        name: "dept_name",
+        message: "What is the department name?",
+      },
+    ]);
+    const data = await db
+      .promise()
+      .query("INSERT INTO department SET ?", roleInfo);
+    console.log("Role succesfully added!");
+    viewAllRoles();
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+menu();
